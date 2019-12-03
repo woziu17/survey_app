dbeebfd102dc35a66f29d2d55fcdb239862df5b8
woziu <agn.woz@gmail.com>
2019-12-03 18:30:28 +0100

    dev and prod keys separated

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: "kjkjhj87y8uh98hh98cd9fe9h39"
+};
--- server/services/passport.js
+++ server/services/passport.js
@@ -4,15 +4,34 @@ const keys = require("../config/keys");
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientId,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
-    (accessToken, refreshToken, profile) => {
-      new User({ googleId: profile.id }).save();
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
